ee1dd4514cfe4ab1ae5db2fa2e271f86754be947
Scott Sharpe <scottsharpe44@gmail.com>
2025-07-29 13:17:54 -0700

    Cocoa Pods and other iPad stuff

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",  // <- This line is critical
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
